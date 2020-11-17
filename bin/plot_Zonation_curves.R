8014762bb011b27f9c850b4eb1920b48b1e34b00
AliciaMstt <ticatla@gmail.com>
2020-11-16 22:16:58 -0600

    plot for Sup Fig 8 with curves from all scenarios

--- bin/plot_Zonation_curves.R
+++ bin/plot_Zonation_curves.R
@@ -10,7 +10,7 @@ library(tidyr)
 library(purrr)
 
 ####               
-#### For all Taxa
+#### For all Taxa (Figure main text)
 ####
 
 ###### Read data
@@ -198,21 +198,30 @@ ggplot(zc_forplot, aes(x=Area_kept, y=prop, color=Category)) +
 
 ####               
 #### Scenarios with all taxa, taxa exclusively distributing in natural vegetation, and taxa associated to different habitats, i.e. natural vegetation, agricultural and urban areas.
-####
+#### (Figure Sup Mats)
 
-#### Falta cambiar lo de abajo a los paths con los archvios nuevos
-E_final_HabVarios.features_info.txt
-E_final_VegPyS.features_info.txt
-
-
+## Function to transform the Zonation curves to the right format for plotting
+generate_zc_forplot<-function(input_features, input_zcurves){
+  ### This function as input the csv of Zonation features and curves,
+  ### as well as the list of IUCN category for each taxa to
+  ### transform them to the right format to plot the curves grouping taxa by IUCN category 
 
 ## Read IUCN category per taxa
-iucn<-read.csv("../data/spatial/Zonation_final_solutions/IUCN_threat_category.csv")
+  iucn<-read.csv("../data/spatial/Zonation_final_solutions/IUCN_threat_category.csv")
+  
+## Read conservation features names
+zfeatures<-read.table(input_features,
+                      skip=1, header = TRUE)
+
+# extract conservation features names (each SDP subdivided by its PDG)
+feature_names<-substring(zfeatures$MapFileName, first= 9) # cut MDP_PDG/
+feature_names<-gsub(pattern=".tif", replacement= "", feature_names) # cut .tif
 
 ## Read Zonation curves output 
-zcurves<-read.table("../data/spatial/Zonation_final_solutions/E_final_Todos.curves.txt",
+zcurves<-read.table(input_zcurves,
                     header=FALSE,
                     comment.char = "#") # firt line is comments, not real col names
+
 
 ###### Tidy Zonation curves output
 
@@ -220,10 +229,6 @@ zcurves<-read.table("../data/spatial/Zonation_final_solutions/E_final_Todos.curv
 colnames(zcurves) <- c("Prop_landscape_lost", "cost_needed_for_top_fraction",
                        "min_prop_rem",  "ave_prop_rem", "W_prop_rem","ext_1", "ext_2", 
                        feature_names) # from column 8 onwards column names correspond to feature names
-
-# Check few entries and cols of data       
-zcurves[1:5, 1:10]
-
 
 ### Tidy data 
 # change to have one column per taxon
@@ -238,8 +243,6 @@ zcurves_tidy<-gather(zcurves, feature, prop, 8:ncol(zcurves)) %>%
   
   # filter species that were not evaluated in iucn workshop
   filter(IUCN.threat.category!="NO EVALUADA") %>% droplevels(.)
-
-head(zcurves_tidy)
 
 ## Estimate area kept for category
 # CR
@@ -280,5 +283,141 @@ VU<-filter(zcurves_tidy, IUCN.threat.category=="VU") %>%
 
 ## Join all together for plot
 zc_forplot<-rbind(CR, EN, DD, LC, NT, VU)
-head(zc_forplot)
+return(zc_forplot)
+}
+
+
+### Generate zonation curves for plot for each dataset
+
+# all
+zc_forplot_all<-generate_zc_forplot(input_features<-"../data/spatial/Zonation_final_solutions/E_final_Todos.features_info.txt",
+                                    input_zcurves<-"../data/spatial/Zonation_final_solutions/E_final_Todos.curves.txt")
+
+# taxa exclusively distributing in natural vegetation
+zc_forplot_Veg<-generate_zc_forplot(input_features<-"../data/spatial/Zonation_final_solutions/E_final_VegPyS.features_info.txt",
+                                    input_zcurves<-"../data/spatial/Zonation_final_solutions/E_final_VegPyS.curves.txt")
+
+# taxa associated to different habitats, i.e. natural vegetation, agricultural and urban areas.
+zc_forplot_HabVarios<-generate_zc_forplot(input_features<-"../data/spatial/Zonation_final_solutions/E_final_HabVarios.features_info.txt",
+                                    input_zcurves<-"../data/spatial/Zonation_final_solutions/E_final_HabVarios.curves.txt")
+
+
+### Plot 
+# colors for Iucn categories
+iucn.cols<-c("#D81E05", "#FC7F3F", "#F9E814", "#CCE226", "#60C659", "#D1D1C6") # respectively for "CR", "EN", "VU", "NT", "LC", "DD"
+
+# plot a) all taxa
+plot.a<-ggplot(zc_forplot_all, aes(x=Area_kept, y=prop, color=Category)) +
+  stat_summary(fun = mean, geom = "line", size=1.5) +
+  scale_color_manual(values= iucn.cols,
+                     breaks= c("CR", "EN", "VU", "NT", "LC", "DD"),
+                     name= "IUCN threat category",
+                     guide = guide_legend(override.aes = list(size = 3))) +
+  # nicer labels
+  labs(x="", y= "") +
+  ggtitle("a)") +
+  
+  # nicer background
+  theme_bw() +
+  
+  # larger text
+  theme(
+    plot.title = element_text(size=16, face="bold"),
+    axis.title = element_text(size=14),
+    axis.text = element_text(size=13),
+    legend.text = element_text(size=13),
+    legend.title = element_text(size=13, face="bold"))
+
+# plot b) taxa exclusively distributing in natural vegetation
+plot.b<-ggplot(zc_forplot_Veg, aes(x=Area_kept, y=prop, color=Category)) +
+  stat_summary(fun = mean, geom = "line", size=1.5) +
+  scale_color_manual(values= iucn.cols,
+                     breaks= c("CR", "EN", "VU", "NT", "LC", "DD"),
+                     name= "IUCN threat category",
+                     guide = guide_legend(override.aes = list(size = 3))) +
+  # nicer labels
+  labs(x="", y= "Mean proportion of the area of each PDG within each taxon") +
+  ggtitle("b)") +
+  
+  # nicer background
+  theme_bw() +
+  
+  # larger text
+  theme(
+    plot.title = element_text(size=16, face="bold"),
+    axis.title = element_text(size=14),
+    axis.text = element_text(size=13),
+    legend.text = element_text(size=13),
+    legend.title = element_text(size=13, face="bold")) 
+
+# plot for taxa associated to different habitats, i.e. natural vegetation, agricultural and urban areas.
+plot.c<-ggplot(zc_forplot_HabVarios, aes(x=Area_kept, y=prop, color=Category)) +
+  stat_summary(fun = mean, geom = "line", size=1.5) +
+  scale_color_manual(values= iucn.cols,
+                     breaks= c("CR", "EN", "VU", "NT", "LC", "DD"),
+                     name= "IUCN threat category",
+                     guide = guide_legend(override.aes = list(size = 3))) +
+  # nicer labels
+  labs(x="Percentage of Mexican territory", y= "") +
+  ggtitle("c)") +
+  
+  # nicer background
+  theme_bw() +
+  
+  # larger text
+  theme(
+    plot.title = element_text(size=16, face="bold"),
+    axis.title = element_text(size=14),
+    axis.text = element_text(size=13),
+    legend.text = element_text(size=13),
+    legend.title = element_text(size=13, face="bold"))
+
+## Multiplot
+
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  # Multiple plot function from http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+  #
+  # ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+  # - cols:   Number of columns in layout
+  # - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+  #
+  # If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+  # then plot 1 will go in the upper left, 2 will go in the upper right, and
+  # 3 will go all the way across the bottom.
+  #
+    library(grid)
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+multiplot(plot.a, plot.b, plot.c, cols=1)
 
